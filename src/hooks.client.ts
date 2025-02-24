fc6e732f16793d93f3ca784a7a39ba698d324d72
Travis Burandt <travis.burandt@gmail.com>
2025-02-24 15:42:21 -0600

    auth 3

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,17 +1,19 @@
 import type { HandleClientError } from "@sveltejs/kit";
-// To use Clerk components:
-import { initializeClerkClient } from "clerk-sveltekit/client";
-// Or for headless mode:
-// import { initializeClerkClient } from 'clerk-sveltekit/headless'
-import { PUBLIC_CLERK_PUBLISHABLE_KEY } from "$env/static/public";
-
-initializeClerkClient(PUBLIC_CLERK_PUBLISHABLE_KEY, {
-  afterSignInUrl: undefined,
-  afterSignUpUrl: undefined,
-  signInUrl: "/auth/sign-in",
-  signUpUrl: "/auth/sign-up",
-});
 
 export const handleError: HandleClientError = async ({ error, event }) => {
   console.error(error, event);
 };
+
+async function initialize() {
+  const { initializeClerkClient } = await import("clerk-sveltekit/client");
+  const { PUBLIC_CLERK_PUBLISHABLE_KEY } = await import("$env/static/public");
+
+  initializeClerkClient(PUBLIC_CLERK_PUBLISHABLE_KEY, {
+    afterSignInUrl: "/33/",
+    afterSignUpUrl: "/33/",
+    signInUrl: "/auth/sign-in",
+    signUpUrl: "/auth/sign-up",
+  });
+}
+
+initialize();
